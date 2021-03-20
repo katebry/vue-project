fe3551c30bf828ca26e551da578ae1a3e3f30909
Kate Bryan <katebryan31@gmail.com>
2021-03-20 19:37:40 +0000

    chore: fixed spelling mistake

--- vue-project/src/pages/favourites.vue
+++ vue-project/src/pages/favourites.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <h1 class="overview-favorite-pokemon__title">
-      Your favorite Pokémon
+    <h1 class="overview-favourite-pokemon__title">
+      Your favourite Pokémon
     </h1>
     <router-link
       class="nes-btn"
@@ -14,11 +14,11 @@
 </template>
 
 <style scoped>
-.overview-favorite-pokemon {
+.overview-favourite-pokemon {
     display: grid;
     grid-template-columns: 1fr 1fr;
 }
-.overview-favorite-pokemon__title {
+.overview-favourite-pokemon__title {
   margin-top: 10px;
   text-align: center;
 }
