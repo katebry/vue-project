1c486faa555209b62789ecf68d00b8c4798bced9
Kate Bryan <katebryan31@gmail.com>
2021-03-22 13:43:54 +0000

    feat: added computed hook to pagination component

--- vue-project/src/components/Pagination.vue
+++ vue-project/src/components/Pagination.vue
@@ -4,57 +4,76 @@ export default {
     maxVisibleButtons: {
       type: Number,
       required: false,
-      default: 3
+      default: 3,
     },
     totalPages: {
       type: Number,
-      required: true
+      required: true,
     },
     total: {
       type: Number,
-      required: true
+      required: true,
     },
     currentPage: {
       type: Number,
-      required: true
-    }
-  }
+      required: true,
+    },
+  },
+  computed: {
+    startPage() {
+      // When on the first page
+      if (this.currentPage === 1) {
+        return 1;
+      }
+      // When on the last page
+      if (this.currentPage === this.totalPages) {
+        return this.totalPages - this.maxVisibleButtons;
+      }
+      // When in between
+      return this.currentPage - 1;
+    },
+    pages() {
+      const range = [];
+
+      for (
+        let i = this.startPage;
+        i <=
+        Math.min(this.startPage + this.maxVisibleButtons - 1, this.totalPages);
+        i += 1
+      ) {
+        range.push({
+          name: i,
+          isDisabled: i === this.currentPage,
+        });
+      }
+
+      return range;
+    },
+  },
 };
 </script>
 <template>
   <ul>
     <li>
-      <button
-        type="button"
-      >
-        First
+      <button type="button">First</button>
+    </li>
+
+    <li>
+      <button type="button">Previous</button>
+    </li>
+
+    <li v-for="page in pages" :key="page.name">
+      <button type="button" :disabled="page.isDisabled">
+        {{ page.name }}
       </button>
     </li>
 
     <li>
-      <button
-        type="button"
-      >
-        Previous
-      </button>
-    </li>
-
-    <!-- Range of pages -->
-
-    <li>
-      <button
-        type="button"
-      >
-        Next
-      </button>
+      <button type="button">Next</button>
     </li>
 
     <li>
-      <button
-        type="button"
-      >
-        Last
-      </button>
+      <button type="button">Last</button>
     </li>
   </ul>
 </template>
